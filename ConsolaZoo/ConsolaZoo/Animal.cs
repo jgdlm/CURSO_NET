6e9ff1b5fc5007cfc55350dab6a325a39e4e714e
jgdlm <proyectoswebjggonzalezdelamiar@gmail.com>
2021-07-16 20:30:22 +0200

    Subiendo clase animal terminado

--- ConsolaZoo/ConsolaZoo/Animal.cs
+++ ConsolaZoo/ConsolaZoo/Animal.cs
@@ -7,6 +7,55 @@ using System.Threading.Tasks;
 namespace ConsolaZoo
 {
     class Animal
-    { //Creamos la clase Animal
+    { 
+        //Creamos la clase Animal
+
+        //Defino las vaariables
+        string especie, nombre;
+        double peso;
+        int jaula;
+
+        //Definimos un constructor
+        public Animal()
+        {
+
+        }
+
+        //GET y SET
+        public string Aespecie
+        {
+            get { return especie; }
+            set { especie = value; }
+        }
+
+
+        public string Anombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public double Apeso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        public int Ajaula
+        {
+            get { return jaula; }
+            set { jaula = value; }
+        }
+
+
+
+        //Metodo ClaseDeAnimalEres()
+
+
+        public virtual void ClaseDeAnimalEres()
+        {
+            Console.WriteLine("La Clase de Animal que eres es:");
+        }
+
     }
 }
