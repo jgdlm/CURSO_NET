c75ec58218a76791185af190748cd4f51f0ac32f
jgdlm <proyectoswebjggonzalezdelamiar@gmail.com>
2021-07-16 16:23:49 +0200

    Insertado Enunciado del ejercicio

--- ConsolaBanco/ConsolaBanco/Program.cs
+++ ConsolaBanco/ConsolaBanco/Program.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace ConsolaBanco
 {
     class Program
-    {
+    { 
+        //Ejercicio para gestionar las cuentas de un banco
+        //Se pide crear una clase tipo cuenta para gestionar ingresos y gastos
         static void Main(string[] args)
         {
         }
