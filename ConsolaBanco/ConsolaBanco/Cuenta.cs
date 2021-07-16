c061dd3e737ab745cc9b3fba21ed482403807d06
jgdlm <proyectoswebjggonzalezdelamiar@gmail.com>
2021-07-16 17:25:13 +0200

    Subiendo variables y contructor

--- ConsolaBanco/ConsolaBanco/Cuenta.cs
+++ ConsolaBanco/ConsolaBanco/Cuenta.cs
@@ -7,7 +7,24 @@ using System.Threading.Tasks;
 namespace ConsolaBanco
 {
     //Hacemos la clase cuenta
-    class Cuenta
+    class Cuenta //public class Libro es igual, si no pones public, actua como public por defecto
     {
+        //se establecen los campos que se necesitan
+        //todos sus campos son privados, accesibles desde su propia clase
+
+        //campos o atributos. privados. protected si hay herencia
+        private int NumCuenta;
+        private string NombreCliente;
+        private decimal Saldo;
+
+       public Cuenta() //constructor por defecto con mismo nombre de la clase
+                       //nos sirve para acceder al objeto creado a través de sus propiedades
+        {
+
+        }
+
+
+
+
     }
 }
