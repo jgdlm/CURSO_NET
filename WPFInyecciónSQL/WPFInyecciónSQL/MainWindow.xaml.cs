c6dab39bbf7f840efc68f21d42a233112e7eccf8
jgdlm <proyectoswebjggonzalezdelamiar@gmail.com>
2021-07-23 20:38:07 +0200

    subo inyectar sql

---
+++ WPFInyecciónSQL/WPFInyecciónSQL/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace WPFInyecciónSQL
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection conexion = new SqlConnection("server = ASATA407\\SQLEXPRESSJORGE; Initial Catalog = INYECCIONSQL; Persist Security Info=true;Pwd=1234;User ID = sa");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInsertar_Click(object sender, RoutedEventArgs e)
+        {
+            conexion.Open();
+            string iden = TxtID.Text;
+            string nombre = TxtNombre.Text;
+            string edad = TxtEdad.Text;
+            string cadena = "insert into Personas(ID,Nombre,Edad)" + "values ("+ iden + ",'" + nombre + ",'" + edad + ")";
+
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Los Datos se guardaron correctamente");
+            TxtID.Text = "";
+            TxtNombre.Text = "";
+            conexion.Close();
+
+        }
+    }
+}
