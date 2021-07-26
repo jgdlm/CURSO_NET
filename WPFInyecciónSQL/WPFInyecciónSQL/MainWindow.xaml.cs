a44cc1ba490e453bc4042aba90528182ddd17a3b
jgdlm <proyectoswebjggonzalezdelamiar@gmail.com>
2021-07-26 15:55:07 +0200

    insertar el using Data.SqlClient

--- WPFInyecciónSQL/WPFInyecciónSQL/MainWindow.xaml.cs
+++ WPFInyecciónSQL/WPFInyecciónSQL/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data.SqlClient;
+using System.Data.SqlClient; //Insertar este using
 
 namespace WPFInyecciónSQL
 {
