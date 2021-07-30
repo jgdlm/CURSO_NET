6b6b2e1ba6cf2298ce43c4401507b8187317fcdd
jgdlm <proyectoswebjggonzalezdelamiar@gmail.com>
2021-07-30 18:52:44 +0200

    SUBIENDO FORMULARIO  PRODUCTO (nUEVO PRODUCTO, ELIMINAR PRODUCTO,MODIFICAR PRODUCTO)LINQ CON LAMDA

---
+++ FORMULARIOLINQYLAMBDA/FORMULARIOLINQYLAMBDA/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace FORMULARIOLINQYLAMBDA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnActualizar = new System.Windows.Forms.Button();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.TxtCat = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtStock = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtPrec = new System.Windows.Forms.TextBox();
+            this.TxtProd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.GridDatos = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnActualizar);
+            this.groupBox1.Controls.Add(this.BtnGuardar);
+            this.groupBox1.Controls.Add(this.TxtCat);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.TxtStock);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.TxtPrec);
+            this.groupBox1.Controls.Add(this.TxtProd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 173);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "NUEVO PRODUCTO";
+            // 
+            // BtnActualizar
+            // 
+            this.BtnActualizar.Location = new System.Drawing.Point(135, 128);
+            this.BtnActualizar.Name = "BtnActualizar";
+            this.BtnActualizar.Size = new System.Drawing.Size(105, 23);
+            this.BtnActualizar.TabIndex = 9;
+            this.BtnActualizar.Text = "Actualizar";
+            this.BtnActualizar.UseVisualStyleBackColor = true;
+            this.BtnActualizar.Click += new System.EventHandler(this.BtnActualizar_Click);
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(18, 128);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(105, 23);
+            this.BtnGuardar.TabIndex = 8;
+            this.BtnGuardar.Text = "Añadir";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // TxtCat
+            // 
+            this.TxtCat.Location = new System.Drawing.Point(135, 87);
+            this.TxtCat.Name = "TxtCat";
+            this.TxtCat.Size = new System.Drawing.Size(100, 20);
+            this.TxtCat.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(142, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Categoria";
+            // 
+            // TxtStock
+            // 
+            this.TxtStock.Location = new System.Drawing.Point(18, 87);
+            this.TxtStock.Name = "TxtStock";
+            this.TxtStock.Size = new System.Drawing.Size(100, 20);
+            this.TxtStock.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(15, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Unidades En Stock";
+            // 
+            // TxtPrec
+            // 
+            this.TxtPrec.Location = new System.Drawing.Point(135, 32);
+            this.TxtPrec.Name = "TxtPrec";
+            this.TxtPrec.Size = new System.Drawing.Size(100, 20);
+            this.TxtPrec.TabIndex = 3;
+            // 
+            // TxtProd
+            // 
+            this.TxtProd.Location = new System.Drawing.Point(18, 33);
+            this.TxtProd.Name = "TxtProd";
+            this.TxtProd.Size = new System.Drawing.Size(100, 20);
+            this.TxtProd.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(142, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Precio Unidad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nuevo producto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(357, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Nombre del producto";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(360, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(501, 56);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(105, 23);
+            this.BtnEliminar.TabIndex = 10;
+            this.BtnEliminar.Text = "Eliminar Producto";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // GridDatos
+            // 
+            this.GridDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridDatos.Location = new System.Drawing.Point(13, 238);
+            this.GridDatos.Name = "GridDatos";
+            this.GridDatos.Size = new System.Drawing.Size(687, 150);
+            this.GridDatos.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(713, 450);
+            this.Controls.Add(this.GridDatos);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "FORMULARIO CON LINQ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnActualizar;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.TextBox TxtCat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtStock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtPrec;
+        private System.Windows.Forms.TextBox TxtProd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.DataGridView GridDatos;
+    }
+}
+
---
+++ FORMULARIOLINQYLAMBDA/FORMULARIOLINQYLAMBDA/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FORMULARIOLINQYLAMBDA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataClasses1DataContext BasedeDatos = new DataClasses1DataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarCombo();
+            cargarGrid();
+        }
+
+        void cargarCombo()
+        {
+            var cargaCombo = from p in BasedeDatos.Products
+                             select p.ProductName;
+            comboBox1.DataSource = cargaCombo;
+        }
+
+        void cargarGrid()
+        {
+            var cargaGrid = from p in BasedeDatos.Products select p;
+            GridDatos.DataSource = cargaGrid;
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            Products MyProduct = new Products();
+            MyProduct.ProductName = TxtProd.Text;
+            MyProduct.UnitPrice = int.Parse(TxtPrec.Text);
+            MyProduct.UnitsInStock = short.Parse(TxtStock.Text);
+            MyProduct.CategoryID = int.Parse(TxtCat.Text);
+            BasedeDatos.Products.InsertOnSubmit(MyProduct);
+            BasedeDatos.SubmitChanges();
+            cargarGrid();
+            cargarCombo();
+
+        }
+
+        private void BtnActualizar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(comboBox1.SelectedItem.ToString());
+            //Sentencia Lambda
+            Products MyProducto = BasedeDatos.Products.Single(p => p.ProductName == comboBox1.SelectedItem.ToString());
+            MyProducto.ProductName = comboBox1.SelectedItem.ToString();
+            MyProducto.UnitPrice = int.Parse(TxtPrec.Text);
+            MyProducto.UnitsInStock = short.Parse(TxtStock.Text);
+            MyProducto.CategoryID = int.Parse(TxtCat.Text);
+            BasedeDatos.SubmitChanges();
+            cargarGrid();
+            cargarCombo();
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {  
+            //Sentencia Lambda
+            Products MyProduct = BasedeDatos.Products.Single(p => p.ProductName == comboBox1.SelectedItem.ToString());
+            BasedeDatos.Products.DeleteOnSubmit(MyProduct);
+            BasedeDatos.SubmitChanges();
+            cargarGrid();
+            cargarCombo();
+        }
+    }
+}
